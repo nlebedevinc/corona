71580403bc869e599678e2c66eb5bfcf3f826eea
nlebedevinc <nlebedevme@gmail.com>
2020-04-30 02:11:41 +0300

    :tada: refactor test with emoji

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app main component', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
